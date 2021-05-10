a37ab62948df9951b75b45b05f871d4a7c1213d5
Reza Athariq <anrienaya@gmail.com>
2021-05-10 12:41:01 +0700

    project card

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="project-card-container">
+    <div class="row">
+        <div class="col-3" style="float: center">
+            <div class="project-card" type="button"> 
+                <p> -- deskripsi project -- deskripsi project-- deskripsi project-- deskripsi project-- deskripsi project-- deskripsi project--deskripsi project-- deskripsi project--deskripsi project-- deskripsi project-- </p>
+                <div class="project-card-top">
+                    <p> Nama Project </p>
+                </div>
+            </div>
+            <br />
+        </div>
+    </div>
+    
+
+   
+</div>
+
+
+</template>
+
+<script>
+export default {
+  name: "ProjectCard",
+};
+</script>
+
+<style lang="css">
+@import "../css/navbar.css";
+</style>
